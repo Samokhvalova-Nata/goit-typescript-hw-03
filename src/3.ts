fd5b8b176710eebf8dc60ffb16dfa36265f1922f
Nataliia Samokhvalova <samohvalova520@gmail.com>
2023-11-10 21:02:23 +0200

    Add task 2 and 3

--- src/3.ts
+++ src/3.ts
@@ -6,34 +6,45 @@
   Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
   Інтерфейс ISpellCaster повинен включати метод castSpell.
 */
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(): void;
+};
 
-// // реалізація класу Wizard
-// class Wizard implements ICharacter, ISpellCaster {
-//   constructor(public name: string, public level: number) {
-//     if (this.level < 1) {
-//       throw new Error('Level too low');
-//     }
-//   }
+interface ISpellCaster {
+  castSpell(): void;
+}
 
-//   introduce(phrase: string): void {
-//     console.log(phrase + ', ' + this.name);
-//   }
 
-//   castSpell(): void {
-//     console.log('Casting a spell, behold my power!');
-//   }
+// реалізація класу Wizard
+class Wizard implements ICharacter, ISpellCaster {
+  constructor(public name: string, public level: number) {
+    if (this.level < 1) {
+      throw new Error('Level too low');
+    }
+  }
 
-//   levelUp(): void {
-//     this.level++;
-//     console.log(`Level up! New level is ${this.level}`);
-//   }
-// }
+  introduce(phrase: string): void {
+    console.log(phrase + ', ' + this.name);
+  }
 
-// // тестування класу
-// const wizard = new Wizard('Merlin', 15);
+  castSpell(): void {
+    console.log('Casting a spell, behold my power!');
+  }
 
-// wizard.introduce('I am the mighty wizard');
-// wizard.castSpell();
-// wizard.levelUp();  // Level up! New level is 16
+  levelUp(): void {
+    this.level++;
+    console.log(`Level up! New level is ${this.level}`);
+  }
+}
 
-// export {};
+// тестування класу
+const wizard = new Wizard('Merlin', 15);
+
+wizard.introduce('I am the mighty wizard');
+wizard.castSpell();
+wizard.levelUp();  // Level up! New level is 16
+
+export {};
